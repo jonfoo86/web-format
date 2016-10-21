8dea3f965829463fb616d1f0f76ecb1f0923a9a7
jonfoo <wjianfa@foxmail.com>
2016-10-22 01:34:06 +0800

    fix bug

--- app/controllers/formattext.js
+++ app/controllers/formattext.js
@@ -1,6 +1,5 @@
 app.controller('FormatTextCtrl', ['$scope', function ($scope) {
     $scope.formatfc = function () {
-        console.log("action");
-        $scope.destext = "ssssssssssssssssssss";
+        $scope.destext =  $scope.orgtext;
     };
 }])
