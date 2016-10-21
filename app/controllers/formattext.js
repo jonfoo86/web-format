37e4fa224f39b4be09a24dadc4ec947823302e53
admin <admin@jonfoodeMacBook-Air.local>
2016-10-22 01:07:54 +0800

    first

---
+++ app/controllers/formattext.js
@@ -0,0 +1,6 @@
+app.controller('FormatTextCtrl', ['$scope', function ($scope) {
+    $scope.formatfc = function () {
+        console.log("action");
+        $scope.destext = "ssssssssssssssssssss";
+    };
+}])
