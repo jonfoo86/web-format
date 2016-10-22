75fbfb3e5040cc1df87ed8e35a3941c68a4a60f2
jonfoo <wjianfa@foxmail.com>
2016-10-22 15:51:03 +0800

    fix

--- app/controllers/formattext.js
+++ app/controllers/formattext.js
@@ -1,5 +1,10 @@
 app.controller('FormatTextCtrl', ['$scope', function ($scope) {
+    $scope.isVisible = false;
+    $scope.class = "col-md-12";
     $scope.formatfc = function () {
+
         $scope.destext =  $scope.orgtext;
+        $scope.isVisible = true;
+        $scope.class = "col-md-6";
     };
 }])
