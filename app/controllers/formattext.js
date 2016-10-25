f735914c67fb21f7328ec09e6727d72c547415ad
jonfoo <wjianfa@foxmail.com>
2016-10-26 00:44:27 +0800

    fix bug

--- app/controllers/formattext.js
+++ app/controllers/formattext.js
@@ -15,6 +15,8 @@ function valiJson(jsonString) {
     return false;
 };
 
+
+
 app.controller('FormatTextCtrl', ['$scope', '$http', function ($scope, $http) {
     $scope.isVisible = false;
     $scope.class = "col-md-12";
@@ -23,7 +25,11 @@ app.controller('FormatTextCtrl', ['$scope', '$http', function ($scope, $http) {
         $scope.class = "col-md-6";
         $scope.destext = "格式化中...";
         if (valiJson($scope.orgtext)) {
-           // $scope.destext = valiJson($scope.orgtext);
+            var result = hljs.highlightAuto($scope.orgtext);
+           // $scope.destext ="<h2>xxxx</h2>";//result.value.toString();
+          // $scope.destext =$scope.orgtext;
+            /****
+            valiJson($scope.orgtext);
             $http.post('/formatJson', {'date': $scope.orgtext})
                 .success(function (data, status, headers, config) {
                     $scope.destext = data;
@@ -31,6 +37,7 @@ app.controller('FormatTextCtrl', ['$scope', '$http', function ($scope, $http) {
                 .error(function () {
                     $scope.destext = "格式化远程调用失败";
                 });
+             ***/
         }
         else {
             $http.post('/vidadateJson', {'date': $scope.orgtext})
@@ -42,4 +49,32 @@ app.controller('FormatTextCtrl', ['$scope', '$http', function ($scope, $http) {
                 });
         }
     };
-}]);
+}]).directive("formatdynamic",  function($compile){
+    alert("x");
+    return{
+        link: function(scope, element){
+            alert(scope.class);
+            var template = "<textarea >{{orgtext}}</textarea>";
+            var linkFn = $compile(template);
+            var content = linkFn(scope);
+            element.append(content);
+        }
+    };
+    /*
+    return {
+        templateUrl: function(elem, attr) {
+            return $scope.orgtext;
+        }
+    };
+
+    return{
+        link: function(scope, element){
+            var template =  hljs.highlightAuto(scope.orgtext);
+            alert(template.value);
+            var linkFn = $compile(template.value);
+            var content = linkFn(scope);
+            element.append(content);
+        }
+    }
+    */
+});
