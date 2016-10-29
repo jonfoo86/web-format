48bb1c1756969a3644fc3abb28257b0ce9bdb240
jonfoo <wjianfa@foxmail.com>
2016-10-30 01:38:00 +0800

    fix bug

--- app/controllers/formattext.js
+++ app/controllers/formattext.js
@@ -19,10 +19,12 @@ function valiJson(jsonString) {
 app.controller('FormatTextCtrl', ['$scope', '$http', '$timeout', '$sce', function ($scope, $http, $timeout, $sce) {
     $scope.isVisible = false;
     $scope.class = "col-md-12";
+    $scope.alltype = ["json格式化", "javascript(js)格式化", "xml格式化", "html格式化", "c++格式化", "自动格式化"];
     $scope.formatfc = function () {
         $scope.isVisible = true;
         $scope.class = "col-md-6";
         $scope.destext = "格式化中...";
+
         if (valiJson($scope.orgtext)) {
             var beautifycode = js_beautify($scope.orgtext);
             var result = hljs.highlightAuto(beautifycode);
