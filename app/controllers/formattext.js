425aefb273d3916075f872489bad77701dfa8bf1
jonfoo <wjianfa@foxmail.com>
2016-12-25 22:12:35 +0800

    fix bug

--- app/controllers/formattext.js
+++ app/controllers/formattext.js
@@ -13,9 +13,16 @@ function valiJson(jsonString) {
 
     return false;
 };
+function looks_like_html(source) {
+    // <foo> - looks like html
+    // <!--\nalert('foo!');\n--> - doesn't look like html
 
+    var trimmed = source.replace(/^[ \t\n\r]+/, '');
+    var comment_mark = '<' + '!-' + '-';
+    return (trimmed && (trimmed.substring(0, 1) === '<' && trimmed.substring(0, 4) !== comment_mark));
+}
 
-app.controller('FormatTextCtrl', ['$scope', '$http', '$timeout', '$sce', '$location',  function ($scope, $http, $timeout, $sce, $location) {
+app.controller('FormatTextCtrl', ['$scope', '$http', '$timeout', '$sce', '$location', function ($scope, $http, $timeout, $sce, $location) {
     $scope.isVisible = false;
     $scope.class = "col-md-12";
     $scope.alltype = [{name: "json格式化", path: "json_format"},
@@ -35,7 +42,7 @@ app.controller('FormatTextCtrl', ['$scope', '$http', '$timeout', '$sce', '$locat
         }
         else {
             elem = document.getElementById("json_format");
-            if (elem)            {
+            if (elem) {
                 elem.className = "active";
             }
             $scope.formatbk = "json_format";
@@ -49,9 +56,32 @@ app.controller('FormatTextCtrl', ['$scope', '$http', '$timeout', '$sce', '$locat
         $scope.destext = "格式化中...";
 
         if (1 || valiJson($scope.orgtext)) {
-            var beautifycode = js_beautify($scope.orgtext);
-            var result = hljs.highlightAuto(beautifycode);
-            var replacestr = result.value.replace(/\n/g, "<br\>");
+            var default_opts = {
+                indent_size: 4,
+                indent_char: ' ',
+                preserve_newlines: true,
+                jslint_happy: false,
+                keep_array_indentation: false,
+                brace_style: 'collapse',
+                space_before_conditional: true,
+                break_chained_methods: false,
+                selector_separator: '\n',
+                end_with_newline: false
+            };
+            var opts = JSON.parse(JSON.stringify(default_opts));
+            var beautifycode = $scope.orgtext;
+            var result = null;
+            if (looks_like_html(beautifycode)) {
+                beautifycode = html_beautify(beautifycode, opts);
+                result = hljs.highlightAuto(beautifycode, ['xml']);
+            }
+            else {
+                beautifycode = js_beautify(beautifycode, opts);
+                result = hljs.highlightAuto(beautifycode, ['json', 'javascript']);
+            }
+
+            var replacestr = result.value.replace(/\r/g, '·')
+                .replace(/\n/g, '<br>');
             $scope.myHTML = $sce.trustAsHtml(replacestr);
         }
         else {
